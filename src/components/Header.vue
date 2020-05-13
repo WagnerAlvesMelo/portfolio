8946ab249a3d217da7b6f85716f22b42effdfe3b
WagnerAlvesMelo <walves13@outlook.com>
2020-05-13 11:46:09 -0300

    header fix

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <nav class="menu">
-      <p class="menu__logo">Meu pirufólico</p>
+      <p class="menu__logo">Meu portfólio</p>
       <ul class="menu__list">
         <li class="menu__list__item">
           <a>Home</a>
